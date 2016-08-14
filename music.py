c9b02338d0f9c132d693e56bf00bb479a5d15f91
Thomas <tpanetti@email.sc.edu>
2016-08-14 09:25:02 -0400

    added updated music.py

--- music.py
+++ music.py
@@ -1,10 +1,11 @@
 from gmusicapi import Mobileclient
 import sys
 import datetime
+import billboard
 
 #Grab username and password from arguments
 if len(sys.argv) != 3:
-	sys.exit("Enter a email and password as arguments [username]@[email] [password]")
+	sys.exit("Enter a email and password as arguments [email] [password]")
 
 
 username = sys.argv[1]
@@ -14,25 +15,23 @@ password = sys.argv[2]
 api = Mobileclient()
 api.login(username, password, Mobileclient.FROM_MAC_ADDRESS)
 
-# library = api.get_all_songs()
-# tracks = [track['id'] for track in library]
-# print(library[0])
-# sys.exit()
 
 
 
-
-#Get this week's dankest music from X source 
+dankTunes = []
+#Get this week's dankest music from X source
+chart = billboard.ChartData('billboard-200')
+for song in chart:
+	dankTunes.append(song.artist + ' ' + song.title)
 #TODO: Pull down popular tunes from a source
-#As a placeholder, we will use Smash Mouth's "All Star"
-dankTunes = ['All Star', 'Do I Wanna Know']
 
 #Search and Create Playlist
 songsToAdd = []
 for tune in dankTunes:
 	song = api.search(tune)
-	id = song.get('song_hits')[0].get('track').get('storeId')
-	songsToAdd.append(id)
+	if len(song.get('song_hits')) > 0: 
+		id = song.get('song_hits')[0].get('track').get('storeId')
+		songsToAdd.append(id)
 
 #If any songs are found, add the found music to the playlist
 if len(songsToAdd) > 0:
